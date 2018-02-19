fe71f9c6a21f73cc8a3a2d56e395069b5b2f5e46
yamunaanisetti <35917477+yamunaanisetti@users.noreply.github.com>
2018-02-19 12:21:24 +0530

    Create wlcome.php

---
+++ wlcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+ Name:<?php echo $_POST["name"]; ?><br>
+  Email:<?php echo $_POST["email"]; ?><br>  
+Phone num:<?php echo $_POST["phone"]; ?><br> 
+Address:<?php echo $_POST["address"]; ?><br>
+</body>
+</html>
