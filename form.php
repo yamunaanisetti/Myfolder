7091a01a2b34494818ddddcedd85ec466067998b
yamunaanisetti <35917477+yamunaanisetti@users.noreply.github.com>
2018-02-19 12:25:28 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+First name:<?php echo $_POST["firstname"]; ?><br>
+Address:<?php echo $_POST["Address"]; ?><br>  
+gender:<?php echo $_POST["gender"]; ?><br> 
+Age:<?php echo $_POST["Age"]; ?><br>
+Phone Number:<?php echo $_POST["PhoneNumber"]; ?><br>
+</body>
+</html>
